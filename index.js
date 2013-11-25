e30ff6f1a2c81ea86f6e59a34ede3f48a6bc2e62
aaron <a.aa.akio.aaron@gmail.com>
2013-11-24 16:31:05 -0800

    Code refactor, cleanup, and comments

--- index.js
+++ index.js
@@ -1,153 +1,195 @@
 var parse = require('esprima').parse
 
-module.exports = function(js, parentContext){
+module.exports = safeEval
+module.exports.eval = safeEval
+module.exports.FunctionFactory = FunctionFactory
+module.exports.Function = FunctionFactory()
 
-  if (typeof js === 'string'){
-    js = parse(js)
-  }
-
+// 'eval' with a controlled environment
+function safeEval(src, parentContext){
+  var tree = prepareAst(src)
   var context = Object.create(parentContext || {})
-  return evaluateAst(js, context)
+  return evaluateAst(tree, context)
 }
 
-module.exports.FunctionFactory = FunctionFactory
-module.exports.Function = FunctionFactory({})
+// create a 'Function' constructor for a controlled environment
+function FunctionFactory(parentContext){
+  var context = Object.create(parentContext || {})
+  return function Function() {
+    // normalize arguments array
+    arguments = Array.prototype.slice.call(arguments)
+    var src = arguments.slice(-1)[0]
+    var args = arguments.slice(0,-1)
+    if (typeof src === 'string'){
+      //HACK: esprima doesn't like returns outside functions
+      src = parse('function a(){' + src + '}').body[0].body
+    }
+    var tree = prepareAst(src)
+    return getFunction(tree, args, context)
+  }
+}
 
+// takes an AST or js source and returns an AST
+function prepareAst(src){
+  var tree = (typeof src === 'string') ? parse(src) : src
+  return tree
+}
+
+// evaluate an AST in the given context
 function evaluateAst(tree, context){
 
   var safeFunction = FunctionFactory(context)
 
-  function wtf(node){
-    console.error(node)
-    throw new Error('Unsupported expression')
+  return walk(tree)
+
+  // walk through each node of a program of block statement
+  function walkBlock(node){
+    var result = null
+    node.body.forEach(function(childNode){
+      if (childNode.type === 'EmptyStatement') return
+      result = walk(childNode)
+    })
+    return result
   }
 
+  // recursively evalutate the node of an AST
   function walk(node){
-    if (node.type == 'Program' || node.type == 'BlockStatement'){
-      var result = null
-      for (var i=0;i<node.body.length;i++){
-
-        if (node.type === 'EmptyStatement') {
-          continue
-        }
-
-        result = walk(node.body[i])
-      }
-      return result
-    } else if (node.type == 'FunctionExpression'){
-      var params = node.params.map(getName)
-      return getFunction(node.body, params, context)
-    } else if (node.type == 'ReturnStatement'){
-      var value = walk(node.argument)
-      return new ReturnValue(value)
-    } else if (node.type == 'ExpressionStatement'){
-      return walk(node.expression)
-    } else if (node.type == 'AssignmentExpression'){
-      return setValue(context, node.left, node.right, node.operator)
-    } else if (node.type == 'UpdateExpression'){
-      return setValue(context, node.argument, null, node.operator)
-    } else if (node.type == 'VariableDeclaration'){
-      node.declarations.forEach(function(declaration){
-        if (declaration.init){
-          context[declaration.id.name] = walk(declaration.init)
-        } else {
-          context[declaration.id.name] = undefined
-        }
-      })
-    } else if (node.type == 'IfStatement'){
-      if (walk(node.test)){
-        return walk(node.consequent)
-      } else {
-        return walk(node.alternate)
-      }
-    } else if (node.type == 'ForStatement'){
-      for (walk(node.init); walk(node.test); walk(node.update)){
-        walk(node.body)
-      }
-    } else if (node.type === 'Literal') {
-      return node.value
-    } else if (node.type === 'UnaryExpression'){
-      var val = walk(node.argument)
-      var op = node.operator;
-      if (op === '+') return +val
-      if (op === '-') return -val
-      if (op === '~') return ~val
-      if (op === '!') return !val
-      return wtf(node)
-    } else if (node.type === 'ArrayExpression') {
-      var xs = [];
-      for (var i = 0, l = node.elements.length; i < l; i++) {
-        var x = walk(node.elements[i]);
-        xs.push(x);
-      }
-      return xs;
-    } else if (node.type === 'ObjectExpression') {
-      var obj = {};
-      for (var i = 0; i < node.properties.length; i++) {
-        var prop = node.properties[i];
-        var value = prop.value === null
-          ? prop.value
-          : walk(prop.value)
-        ;
-        obj[prop.key.value || prop.key.name] = value;
-      }
-      return obj;
-    } else if (node.type === 'BinaryExpression') {
-      var l = walk(node.left);
-      var r = walk(node.right);
+    switch (node.type) {
       
-      var op = node.operator;
-      if (op === '==') return l == r;
-      if (op === '===') return l === r;
-      if (op === '!=') return l != r;
-      if (op === '!==') return l !== r;
-      if (op === '+') return l + r;
-      if (op === '-') return l - r;
-      if (op === '*') return l * r;
-      if (op === '/') return l / r;
-      if (op === '%') return l % r;
-      if (op === '<') return l < r;
-      if (op === '<=') return l <= r;
-      if (op === '>') return l > r;
-      if (op === '>=') return l >= r;
-      if (op === '|') return l | r;
-      if (op === '&') return l & r;
-      if (op === '^') return l ^ r;
-      if (op === '&&') return l && r;
-      if (op === '||') return l || r;
-
-      return wtf(node)
-    } else if (node.type == 'ThisExpression'){
-      return context['this']
-    } else if (node.type === 'Identifier') {
-      return context[node.name]
-    } else if (node.type === 'CallExpression') {
-      var args = [];
-      for (var i = 0, l = node.arguments.length; i < l; i++) {
-        var x = walk(node.arguments[i]);
-        args.push(x);
-      }
-      var object = null
-      var target = walk(node.callee)
-      if (node.callee.type === 'MemberExpression'){
-        object = walk(node.callee.object)
-      }
-      return target.apply(object, args)
-    } else if (node.type === 'MemberExpression') {
-      var obj = walk(node.object);
-      if (node.computed){
-        var prop = walk(node.property)
-        return checkValue(obj[prop])
-      } else {
-        return checkValue(obj[node.property.name]);
-      }
-    } else if (node.type === 'ConditionalExpression') {
-      var val = walk(node.test)
-      return val ? walk(node.consequent) : walk(node.alternate)
+      case 'Program':
+        return walkBlock(node)
+      
+      case 'BlockStatement':
+        return walkBlock(node)
+      
+      case 'FunctionExpression':
+        var params = node.params.map(getName)
+        return getFunction(node.body, params, context)
+      
+      case 'ReturnStatement':
+        var value = walk(node.argument)
+        return new ReturnValue(value)
+      
+      case 'ExpressionStatement':
+        return walk(node.expression)
+      
+      case 'AssignmentExpression':
+        return setValue(context, node.left, node.right, node.operator)
+      
+      case 'UpdateExpression':
+        return setValue(context, node.argument, null, node.operator)
+      
+      case 'VariableDeclaration':
+        node.declarations.forEach(function(declaration){
+           if (declaration.init){
+            context[declaration.id.name] = walk(declaration.init)
+          } else {
+            context[declaration.id.name] = undefined
+          }
+        })
+        break
+      
+      case 'IfStatement':
+        if (walk(node.test)){
+          return walk(node.consequent)
+        } else {
+          return walk(node.alternate)
+        }
+      
+      case 'ForStatement':
+        for (walk(node.init); walk(node.test); walk(node.update)){
+          walk(node.body)
+        }
+        break
+      
+      case 'Literal':
+        return node.value
+      
+      case 'UnaryExpression':
+        var val = walk(node.argument)
+        switch(node.operator) {
+          case '+': return +val
+          case '-': return -val
+          case '~': return ~val
+          case '!': return !val
+          default: return unsupportedExpression(node)
+        }
+      
+      case 'ArrayExpression':
+        return node.elements.map(function(element){
+          return walk(element)
+        })
+      
+      case 'ObjectExpression':
+        var obj = {}
+        for (var i = 0; i < node.properties.length; i++) {
+          var prop = node.properties[i]
+          var value = (prop.value === null) ? prop.value : walk(prop.value)
+          obj[prop.key.value || prop.key.name] = value
+        }
+        return obj
+      
+      case 'BinaryExpression':
+        var l = walk(node.left)
+        var r = walk(node.right)
+        switch(node.operator) {
+          case '==':  return l === r
+          case '===': return l === r
+          case '!=':  return l != r
+          case '!==': return l !== r
+          case '+':   return l + r
+          case '-':   return l - r
+          case '*':   return l * r
+          case '/':   return l / r
+          case '%':   return l % r
+          case '<':   return l < r
+          case '<=':  return l <= r
+          case '>':   return l > r
+          case '>=':  return l >= r
+          case '|':   return l | r
+          case '&':   return l & r
+          case '^':   return l ^ r
+          case '&&':  return l && r
+          case '||':  return l || r
+          default: return unsupportedExpression(node)
+        }
+      
+      case 'ThisExpression':
+        return context['this']
+      
+      case 'Identifier':
+        return context[node.name]
+      
+      case 'CallExpression':
+        var args = node.arguments.map(function(arg){
+          return walk(arg)
+        })
+        var object = null
+        var target = walk(node.callee)
+        if (node.callee.type === 'MemberExpression'){
+          object = walk(node.callee.object)
+        }
+        return target.apply(object, args)
+      
+      case 'MemberExpression':
+        var obj = walk(node.object)
+        if (node.computed){
+          var prop = walk(node.property)
+          return checkValue(obj[prop])
+        } else {
+          return checkValue(obj[node.property.name]);
+        }
+      
+      case 'ConditionalExpression':
+        var val = walk(node.test)
+        return val ? walk(node.consequent) : walk(node.alternate)
+      
+      default:
+        return unsupportedExpression(node)
     }
-    else return wtf(node);
   }
 
+  // safely retrieve a value
   function checkValue(value){
     if (value === Function){
       value = safeFunction
@@ -155,16 +197,15 @@ function evaluateAst(tree, context){
     return value
   }
 
+  // set a value in the specified context if allowed
   function setValue(object, left, right, operator){
     var name = null
 
-    if (left.type == 'Identifier'){
+    if (left.type === 'Identifier'){
       name = left.name
-
       // handle parent context shadowing
       object = objectForKey(object, name)
-
-    } else if (left.type == 'MemberExpression'){
+    } else if (left.type === 'MemberExpression'){
       if (left.computed){
         name = walk(left.property)
       } else {
@@ -175,24 +216,27 @@ function evaluateAst(tree, context){
 
     // stop built in properties from being able to be changed
     if (canSetProperty(object, name)){
-      if (!operator || operator === '='){
-        return object[name] = walk(right)
-      } else if (operator === '+='){
-        return object[name] += walk(right)
-      } else if (operator === '-='){
-        return object[name] -= walk(right)
-      } else if (operator === '++'){
-        return object[name]++
-      } else if (operator === '--'){
-        return object[name]--
+      switch(operator) {
+        case undefined: return object[name] = walk(right)
+        case '=':  return object[name] = walk(right)
+        case '+=': return object[name] += walk(right)
+        case '-=': return object[name] -= walk(right)
+        case '++': return object[name]++
+        case '--': return object[name]--
       }
     }
 
   }
 
-  return walk(tree)
 }
 
+// when an unsupported expression is encountered, throw an error
+function unsupportedExpression(node){
+  console.error(node)
+  throw new Error('Unsupported expression')
+}
+
+// walk a provided object's prototypal hierarchy to retrieve an inherited object
 function objectForKey(object, key){
   var proto = Object.getPrototypeOf(object)
   if (!proto || object.hasOwnProperty(key)){
@@ -202,6 +246,7 @@ function objectForKey(object, key){
   }
 }
 
+// determine if we have write access to a property
 function canSetProperty(object, property){
   if (property === '__proto__'){
     return false
@@ -222,6 +267,7 @@ function canSetProperty(object, property){
   }
 }
 
+// generate a function with specified context
 function getFunction(body, params, parentContext){
   return function(){
     var context = Object.create(parentContext)
@@ -230,12 +276,15 @@ function getFunction(body, params, parentContext){
     } else {
       context['this'] = this
     }
+    // normalize arguments array
+    arguments = Array.prototype.slice.call(arguments)
     context['arguments'] = arguments
-    for (var i=0;i<arguments.length;i++){
-      if (params[i]){
-        context[params[i]] = arguments[i]
+    arguments.forEach(function(arg,idx){
+      param = params[idx]
+      if (param){
+        context[param] = arg
       }
-    }
+    })
     var result = evaluateAst(body, context)
     if (result instanceof ReturnValue){
       return result.value
@@ -243,24 +292,12 @@ function getFunction(body, params, parentContext){
   }
 }
 
+// get the name of an identifier
 function getName(identifier){
   return identifier.name
 }
 
+// a ReturnValue struct for differentiating between 'undefined' and 'no return' (?)
 function ReturnValue(value){
   this.value = value
-}
-
-function FunctionFactory(context){
-  return function(){
-    var js = Array.prototype.slice.call(arguments, -1)[0]
-    var args = Array.prototype.slice.call(arguments, 0, -1)
-
-    if (typeof js === 'string'){
-      //HACK: esprima doesn't like returns outside functions
-      js = parse('function a(){' + js + '}').body[0].body
-    }
-
-    return getFunction(js, args, context)
-  }
 }
